4687395df96bfba6c8ee1f86ac7a7dbf6b5c3c7b
Stephen Woods <stephen.woods@salesforce.com>
2018-08-05 13:50:31 -0700

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import './scss/index.scss';
 
+let lastInt = 0;
 const intergers = require('./integers');
 const uuid = require('./uuid');
 function handleClick(e) {
     intergers.getInt().then(resp => {
        document.querySelector('.out').innerText = resp;
+       lastInt = resp;
        document.querySelector('.field').classList.remove('is-invisible');
     }).catch(e => {
         console.error(e);
@@ -14,6 +16,17 @@ function handleClick(e) {
     })
 }
 
-handleClick();
+function handleCopy(e) {
+    navigator.clipboard.writeText(lastInt).then(() => {
+        let ok = document.createElement('span');
+        ok.classList.add('tag', 'is-success');
+        ok.innerText= 'copied!';
+        //<span class="tag is-success">Success</span>
+        document.querySelector('.out').appendChild(ok);
+    });
+}
 
+handleClick();
+// copybtn
+document.querySelector('.copybtn').addEventListener('click', handleCopy);
 document.getElementById('doit').addEventListener('click', handleClick);
